382838d63f758832ab5ca5f1dd41bb1e6ff4a640
Fabien Treand <ftreand@z1r7p4.le-101.fr>
2018-03-20 16:44:46 +0100

    depot

--- ft_ls2/ft_recursive.c
+++ ft_ls2/ft_recursive.c
@@ -6,7 +6,7 @@
 /*   By: ftreand <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/03/07 17:21:16 by ftreand      #+#   ##    ##    #+#       */
-/*   Updated: 2018/03/19 20:03:37 by ftreand     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/20 16:38:13 by ftreand     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,9 +19,8 @@ void	ft_fill_recursive_stats(t_ls **begin, t_ls **ls, DIR *dir, t_flags *fg)
 	t_ls	*recu;
 	t_ls	*now;
 	t_fill	fill;
-//	t_ls	*padd;
 
-	printf("lol d_name = %s\n", (*begin)->d_name); 
+	recu = NULL;
 	while ((fill.dirent = readdir(dir)) != NULL)
 	{
 		if (!recu)
@@ -48,7 +47,6 @@ void	ft_fill_recursive_stats(t_ls **begin, t_ls **ls, DIR *dir, t_flags *fg)
 			ft_sort_list(&(*begin), recu, (*fg));
 		}
 	}
-
 }
 
 void	ft_recup_recu_path(char *ret, char *path, char *name)
@@ -65,28 +63,32 @@ void	ft_recursive(t_ls **ls, t_flags fg, char *path)
 	t_ls	*padd;
 	DIR *dir;
 
-	begin = NULL;
-	printf("ls d_name = %s\n", (*ls)->d_name);
+//	printf("ls d_name = %s\n", (*ls)->d_name);
 	while (ls)
 	{
-		printf("ls type = %d\n", (*ls)->type);
-		printf("ls name = %s\n", (*ls)->d_name);
+//		printf("ls type = %d\n", (*ls)->type);
+//		printf("ls name = %s\n", (*ls)->d_name);
 		if ((*ls)->type == 4 && (*ls)->d_name[0] != '.')
 		{
 			fg.total = 0;
 			dir = opendir((*ls)->d_name);
 			ft_recup_recu_path((*ls)->path, path, (*ls)->d_name);
-			printf("full path = %s\n", (*ls)->path);
+//			printf("full path = %s\n", (*ls)->path);
 			ft_fill_recursive_stats(&begin, &(*ls), dir, &fg);
-			begin = begin->next;
-			printf("begin d_name = %s\n", begin->d_name);
 			padd = begin;
+			begin = begin->next;
+//			printf("begin d_name = %s\n", begin->d_name);
 			pad = ft_padding(&padd, ft_strlen);
 			ft_display(begin, &fg, pad);
-			printf("fucking d_name = %s\n", begin->d_name);
+			printf("lk = %d\n", pad->lk);
+			printf("pw = %zu\n", pad->pw);
+			printf("gr = %zu\n", pad->gr);
+			printf("size = %d\n", pad->size);
+//			printf("fucking d_name = %s\n", begin->d_name);
 //			begin = NULL;
 			closedir(dir);
-//			ft_recursive(&begin, fg, (*ls)->d_name);
+			printf("ls d_name = %s\n", (*ls)->d_name);
+			ft_recursive(&begin, fg, (*ls)->d_name);
 		}
 		(*ls) = (*ls)->next;
 	}
